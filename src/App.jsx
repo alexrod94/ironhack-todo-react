d73f347461408fff5e353095c776ecdb4b95b935
alexrod94 <alexrod94@gmail.com>
2023-05-27 13:15:23 +0200

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,36 @@ function App() {
   const addTodo = () => {};
 
   return (
-    <div>
-      <h1>Todo App</h1>
-      {todoList.map((todo) => {
-        return <Todo key={todo.id} title={todo.title} />;
-      })}
-      <button onClick={() => setOpenForm(!openForm)}>Create a new todo</button>
+    <div className="w-[90%] mx-auto">
       {openForm && (
-        <div>
-          <input type="text" id="newTodo" />
-          <button onClick={() => addTodo()}>Save Todo</button>
+        <div className="flex w-[100%] justify-center my-3">
+          <input
+            className="input input-bordered input-primary w-full max-w-xs bg-white text-black"
+            type="text"
+            id="newTodo"
+          />
+          <button
+            className="btn btn-success text-white"
+            onClick={() => addTodo()}
+          >
+            Save Todo
+          </button>
         </div>
       )}
+      <h1 className="text-center text-white text-3xl my-3">Todo App</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {todoList.map((todo) => {
+          return <Todo key={todo.id} title={todo.title} />;
+        })}
+      </div>
+      <div className="flex justify-center w-[100%] mt-10">
+        <button
+          className="btn btn-primary"
+          onClick={() => setOpenForm(!openForm)}
+        >
+          {openForm ? "Close Form" : "Create Todo"}
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,10 +8,21 @@ function Todo({ title }) {
   };
 
   return (
-    <div>
-      {isCompleted ? <h3 className="completed">{title}</h3> : <h3>{title}</h3>}
-      <button onClick={() => toggleComplete()}>Complete</button>
-      <button>Delete</button>
+    <div className="card bg-white p-4 flex flex-col items-center">
+      {isCompleted ? (
+        <h3 className="completed text-black font-semibold">{title}</h3>
+      ) : (
+        <h3 className="text-black font-semibold">{title}</h3>
+      )}
+      <div className="flex justify-center mt-3">
+        <button
+          className="btn btn-xs mr-2 btn-success text-white"
+          onClick={() => toggleComplete()}
+        >
+          Complete
+        </button>
+        <button className="btn btn-xs ml-2 btn-error text-white">Delete</button>
+      </div>
     </div>
   );
 }
